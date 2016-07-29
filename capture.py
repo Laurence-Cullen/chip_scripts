aaeefd7061148b6008ae3fcb0dd14e4f357aee05
Laurence-Cullen <laurencecullen@live.co.uk>
2016-07-29 11:49:27 +0100

    file clean up

--- capture.py
+++
@@ -1,7 +0,0 @@
-import urllib
-url_choice = 'http://bl24i-di-serv-01.diamond.ac.uk:8082/cam3.MJPG.jpgh/axis-cgi/jpg/image.cgi?resolution=4CIF'
-current_image = urllib.urlopen(url_choice)
-fid = 'laurence.jpg'
-g = open(fid, 'w')
-g.write(current_image.read())
-g.close()
